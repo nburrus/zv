06be379a85404fb79d8ae0af1cfaacc54305746a
Nicolas Burrus <nicolas@burrus.name>
2022-03-31 14:24:26 +0200

    znet: fix a bug when we send too much data in a row.

--- client/znet.h
+++ client/znet.h
@@ -559,13 +559,17 @@ ZN_API void zn_close(zn_State *S) {
 
 ZN_API int zn_run(zn_State *S, int mode) {
     int err;
+    // Never block if there are still pending results to process.
+    // This can happen e.g if we send a ton of stuff in a row, 
+    // hitting the ZN_MAX_RESULT_LOOPS limit.
+    int force_check_only = znQ_empty(&S->result_queue) ? 0 : 1;
     switch (mode) {
     case ZN_RUN_CHECK:
         return znS_poll(S, 1);
     case ZN_RUN_ONCE:
-        return znS_poll(S, 0);
+        return znS_poll(S, force_check_only);
     case ZN_RUN_LOOP:
-        while ((err = znS_poll(S, 0)) > 0)
+        while ((err = znS_poll(S, force_check_only)) > 0)
             ;
         return err;
     }
