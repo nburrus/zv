f6931ce7d44e6cb4076ee345cdb13c664ed03a15
Nicolas Burrus <nicolas@burrus.name>
2022-12-02 16:59:00 +0100

    Fix jpeg image saving.

--- libzv/Image_stb.cpp
+++ libzv/Image_stb.cpp
@@ -18,19 +18,23 @@
 namespace zv
 {    
 
+
     inline bool ends_with(std::string const & value, std::string const & ending)
     {
         if (ending.size() > value.size()) return false;
         return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
     }
 
+    inline bool fileHasJpegExtension (const std::string& fname)
+    {
+        std::string lowerFilename = fname;
+        std::transform(lowerFilename.begin(), lowerFilename.end(), lowerFilename.begin(), [](unsigned char c){ return std::tolower(c); });
+        return ends_with(lowerFilename, ".jpg") || ends_with(lowerFilename, ".jpeg");
+    }
+
     bool readImageFile (const std::string& inputFileName, ImageSRGBA& outputImage)
     {
-        std::string lowerFilename = inputFileName;
-        std::transform(lowerFilename.begin(), lowerFilename.end(), lowerFilename.begin(), [](unsigned char c){ return std::tolower(c); });
-
-        if (ends_with(lowerFilename, ".jpg") 
-            || ends_with(lowerFilename, ".jpeg"))
+        if (fileHasJpegExtension(inputFileName))
         {
             return readJpegFile (inputFileName, outputImage);
         }
@@ -87,9 +91,48 @@ namespace zv
 
         return true;
     }
+
+    bool writeJpegFile (const std::string& filePath, const ImageSRGBA& image)
+    {
+        // Save image with turbojpeg
+        static thread_local tjhandle tjcompressor = nullptr;
+        if (!tjcompressor)
+        {
+            tjcompressor = tjInitCompress();
+        }
+        zv_assert (tjcompressor != nullptr, "Could not initialize a compressor");
+
+        unsigned char* jpegBuf = nullptr;
+        unsigned long jpegSize = 0;
+        int ret = tjCompress2 (tjcompressor,
+                           image.rawBytes(), image.width(), image.bytesPerRow(), image.height(), TJPF_RGBA,
+                           &jpegBuf, &jpegSize, TJSAMP_444, 90, 0);
+        if (ret < 0)
+        {
+            zv_dbg ("Failed to compress");
+            return false;
+        }
+
+        // Save to disk
+        std::ofstream file (filePath, std::ios::binary | std::ios::trunc);
+        if (!file.good())
+        {
+            zv_dbg ("Failed to open file for writing");
+            return false;
+        }
+        file.write ((char*)jpegBuf, jpegSize);
+
+        tjFree (jpegBuf);
+        return true;
+    }
     
     bool writeImageFile (const std::string& filePath, const ImageSRGBA& image)
     {
+        if (fileHasJpegExtension(filePath))
+        {
+            return writeJpegFile (filePath, image);
+        }
+        
         return stbi_write_png(filePath.c_str(), image.width(), image.height(), 4, image.data(), image.bytesPerRow());
     }
     
